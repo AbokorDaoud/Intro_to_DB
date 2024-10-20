69830d44b662a314d79e5b9a4275acb05d1288f2
Abokor Daoud <abokordaoud22@gmail.com>
2024-10-20 05:36:23 +0300

    commit for the updated task4

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,13 @@
 
--- Use the alx_book_store database
-USE alx_book_store;
-
--- Show the structure of the books table
-SELECT COLUMN_NAME, COLUMN_TYPE, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_NAME = 'Books'
+    AND TABLE_SCHEMA = 'alx_book_store';
